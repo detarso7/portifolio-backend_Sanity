4345b3d63ce05c1f936f5c9ec5fd6e38d6a89dae
Saulo de Tarso <de_tarso7@hotmail.com>
2023-04-09 17:14:55 -0300

    Mostrando dados da requisição no header

--- schemas/header.js
+++ schemas/header.js
@@ -4,8 +4,8 @@ export default {
     type: 'document',
     fields: [
         {name:'image', title:'Image', type:'image'},
-        { name: 'name', title: 'Name', type: 'string' },
-        { name: 'ocupation', title: 'Ocupation', type: 'string' },
-        { name: 'summary', title: 'Summary', type: 'text' }
+        {name:'name', title:'Name', type:'string'},
+        {name:'ocupation', title:'Ocupation', type:'string'},
+        {name:'summary', title:'Summary', type:'text'}
     ]
 }
