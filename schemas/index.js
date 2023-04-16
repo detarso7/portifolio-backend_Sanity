a59a83e483bc163e1e51734a7390ca598b79b33b
Saulo de Tarso <de_tarso7@hotmail.com>
2023-04-16 17:22:48 -0300

    Criando schema Projects

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,6 @@
 import about from "./about";
 import header from "./header";
+import projects from "./projects";
 import skills from "./skills";
 
-export const schemaTypes = [header, about, skills]
+export const schemaTypes = [header, about, skills, projects]
