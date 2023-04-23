504edb2e23535918df24f366472e8dbaa35eef3e
Saulo de Tarso <de_tarso7@hotmail.com>
2023-04-23 16:40:05 -0300

    Criando eschema footer e contact

---
+++ schemas/contact.js
@@ -0,0 +1,10 @@
+export default {
+    name: 'contact',
+    title: 'Contact',
+    type: 'document',
+    fields: [
+        {name: 'name', title: 'Name', type: 'string'},
+        {name: 'email', title: 'E-mail', type: 'string'},
+        {name: 'text', title: 'Text', type: 'text'}
+    ]
+}
--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,8 @@
 import about from "./about";
+import contact from "./contact";
+import footer from "./footer";
 import header from "./header";
 import projects from "./projects";
 import skills from "./skills";
 
-export const schemaTypes = [header, about, skills, projects]
+export const schemaTypes = [header, about, skills, projects, contact, footer]
